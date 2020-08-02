6027eb2c7d75ddb5d8ca0b816a3fb113b166c294
Vishal Kumawat <vikumawa@microsoft.com>
2020-08-02 17:02:22 +0530

    adding timeout to handle key up

--- src/LoginLayout/InputLayout.js
+++ src/LoginLayout/InputLayout.js
@@ -5,6 +5,7 @@ import { containerStyles, getNameTextStyles, getCodeTextStyles, getButtonStyles 
 function InputLayout(props) {
   const [name, setName] = useState("");
   const [code, setCode] = useState("");
+  let timeout = 0;
 
   function handleCreate () {
     props.onGameCreate(name);
@@ -14,10 +15,17 @@ function InputLayout(props) {
     props.onGameFetch(name, code);
   }
 
+  function handleKeyUp(data) {
+    const val  = data.target.value
+    if(timeout) clearTimeout(timeout); 
+    timeout = setTimeout(() => setCode(val), 500)
+  }
+
+  console.log("timeout" + timeout);
   return (
       <div style={containerStyles()}>
         <TextField label="Name" styles={getNameTextStyles()} placeholder="Enter text here" onBlur={(data) => setName(data.target.value)} />
-        <TextField label="Game Code" styles={getCodeTextStyles()} placeholder="Enter code here" onKeyUp={(data) => setCode(data.target.value)}/>
+        <TextField label="Game Code" styles={getCodeTextStyles()} placeholder="Enter code here" onKeyUp={(data) => {handleKeyUp(data)}}/>
         <Button disabled={name === ""} text={code ? "Join Room" : "Create Room"} styles={getButtonStyles()}  onClick={code ? handleJoin : handleCreate}/>
       </div>
   );
