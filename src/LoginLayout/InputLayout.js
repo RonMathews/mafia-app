7dee70970bf2fe6f04a0ef7bfd0476f90dd45d35
Vishal Kumawat <vikumawa@microsoft.com>
2020-08-02 17:28:54 +0530

    button issue

--- src/LoginLayout/InputLayout.js
+++ src/LoginLayout/InputLayout.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { TextField, Button } from '@fluentui/react';
 import { containerStyles, getNameTextStyles, getCodeTextStyles, getButtonStyles } from "./InputLayoutStyles";
 
-function InputLayout(props) {
+  function InputLayout(props) {
   const [name, setName] = useState("");
   const [code, setCode] = useState("");
   let timeout = 0;
@@ -15,17 +15,16 @@ function InputLayout(props) {
     props.onGameFetch(name, code);
   }
 
-  function handleKeyUp(data) {
+  function handleKeyUp(data, fun) {
     const val  = data.target.value
     if(timeout) clearTimeout(timeout); 
-    timeout = setTimeout(() => setCode(val), 500)
+    timeout = setTimeout(fun(val), 500)
   }
-
-  console.log("timeout" + timeout);
+ 
   return (
       <div style={containerStyles()}>
-        <TextField label="Name" styles={getNameTextStyles()} placeholder="Enter text here" onBlur={(data) => setName(data.target.value)} />
-        <TextField label="Game Code" styles={getCodeTextStyles()} placeholder="Enter code here" onKeyUp={(data) => {handleKeyUp(data)}}/>
+        <TextField label="Name" styles={getNameTextStyles()} placeholder="Enter text here" onKeyUp={(data) => {handleKeyUp(data, (val) => setName(val))}} />
+        <TextField label="Game Code" styles={getCodeTextStyles()} placeholder="Enter code here" onKeyUp={(data) => {handleKeyUp(data, (val) => setCode(val))}}/>
         <Button disabled={name === ""} text={code ? "Join Room" : "Create Room"} styles={getButtonStyles()}  onClick={code ? handleJoin : handleCreate}/>
       </div>
   );
